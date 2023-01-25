78e68e53e66f38856ba3883cea7095c97bda835c
wcward11 <wcward3302@eagle.fgcu.edu>
2023-01-24 19:15:21 -0500

    In class stuff

---
+++ In Class/1-24-23InClass.cpp
@@ -0,0 +1,26 @@
+/* algorithms 
+
+1. 
+
+X * Y
+for i; i <= y ; i++
+x = x + x
+
+
+
+2. 
+
+x * y
+if x % 10 != 0, trunc ( x % 10)
+
+
+
+*/
+#include <iostream>
+#include <cmath>
+
+int main(){
+    int x = 37;
+    std::cout << (x % 10) << "\n";
+    return 0;
+}
