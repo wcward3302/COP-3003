a3c52630353c28f49acfb9e13a9904279dfa9ed6
wcward3302 <wcward3302@eagle.fgcu.edu>
2023-02-14 22:28:32 -0500

    Assignment 4 nearly complete

--- assignments/assignment4/gradeFunction.cpp
+++ assignments/assignment4/gradeFunction.cpp
@@ -1,108 +1,121 @@
 /*
-
-Make a header file called gradefunctions
-    you can use whichever naming convention you like for this and either .h or .hpp for the extension
-Create and make a gradefunction.cpp file with any logic needed due to the prototypes in the header
-Our grades array (whatever you named it) can be used with this without being global but is not really complete
-    Because the scope can be limited by adding it to a file - we can set it up in one of them
-    So add it to the header or cpp file
-        think about which it should be in to limit the scope best
-        there is a correct place to put it
-Add one struct for a Student (will put in same scope as array)
-    Should have members for gradelevel, name (full), final_grade (the final average), and final_letter_grade (the letter grade)
-    can name these in any way that meets style guide
-Add functions for getting student data and adding to struct
-    Including storing the final average as that member
-    These should be external functions from struct at this point
-Code should function in same was as before but final message should include name
-    "Thank you, <name here>, the final grade for semester is a B at 89%"
-
+Name: William Ward
+Course: COP3003 (CRN15015) - Programming II
+Info: Assignment 3 - gradeFunction.cpp
 */
 
-#include <string>
-#include <array>
-#include <iostream>
 #include "gradeFunction.hpp"
 
-int main(){
+// void setting data functions
+void STUDENT::add_name(std::string full_name){
+    name = full_name;
+}
 
-    // put array here because we dont need other cpp files that access the .hpp to access this array
-    std::array<int, 11> user_input_array;
+void STUDENT::add_grade_level(std::string x_grade_level){
+    grade_level = x_grade_level;
+}
 
+void STUDENT::add_final_grade(int x_final_grade){
+    final_grade = x_final_grade;
+}
 
-    STUDENT student; // create instance 
-    std::string name, grade_level;
+void STUDENT::add_final_letter_grade(char x_final_grade_letter){
+    final_letter_grade = x_final_grade_letter;
+}
 
-    std::cout << "Please enter name: "; // get name
-    std::getline(std::cin, name);
-    student.add_name(name);
+// return info functions
+std::string STUDENT::get_name(){
+    return name;
+}
 
-    std::cout << "Please enter grade level: "; // get grade_level
-    std::getline(std::cin, grade_level);
-    student.add_grade_level(grade_level);
+std::string STUDENT::get_grade_level(){
+    return grade_level;
+}
+
+int STUDENT::get_final_grade(){
+    return final_grade;
+}
+
+char STUDENT::get_final_letter_grade(){
+    return final_letter_grade;
+}
+
+// new function to display message and collect input from user 
+std::string display_and_collect(std::string message){
+    std::string input;
+    std::cout << message;
+    std::getline(std::cin, input);
+
+    return input;
+}
+
+// quit grading based on user input
+void quit_grading (int x){
+
+    // switch based on input from main, once finished, the program will exit
+    switch (x){
+    case -1:
+        // reason -1
+        std::cout << "an error has occurred, please try again.\n";
+        break;
+    case 0:
+        // reason 0, terminate
+        std::cout << "Thank you for using our product!\n";
+        exit (1);
+    case 1:
+        // reason 1
+        std::cout << "Sorry you had to quit early. Thank you for using out product!\n";
+        exit (1);
+        break;
+    default: // default case just skips the switch
+        break;
+    }
+}
+
+// is called to get a grade, handles input and validation before returning value
+int get_grade (){
 
     // initialize variables
-    int loop_counter = 0, average_grade = 0;
-    std::string user_selection;
+    float user_input = 0;
+    bool user_input_valid = false;
 
-    // ask user if they want to input all grades now or specify on a per-grade basis (I DEFINITELY did not add this to make testing easier...)
-    std::cout << "Do you want to enter all grades now? Yes to continue - ";
-    std::cin >> user_selection;
+    // loop will be false until user input is validated and passed true flag
+    while(!user_input_valid){ 
+        std::cout << "Enter grade: ";
+        std::cin >> user_input;
 
-    std::cout << "Enter grades - lowest score will be dropped!\n";
+        // if the input is within range and the cin was not passed bad input, pass true and break from nested while loop
+        if(user_input >= 0 && user_input <= 100 && std::cin.good()){ 
+            user_input_valid = true;
+        }
 
-    // if user wants to enter all grades now...
-    if (char(user_selection[0]) == 'y' || char(user_selection[0] == 'Y')){
-        while (loop_counter < 11){
-            user_input_array[loop_counter] = get_grade_all();
-            loop_counter++;
+        // else, display a message, clear cin buffer and data, repeat loop until response is valid. Stay on target... 
+        else{
+            std::cout << "please enter a valid response (0-100)\n";
+            std::cin.clear();
+            std::cin.ignore(100,'\n');
         }
     }
 
-    // use function where user will specify they want to continue on a per-grade basis
-    else{
-        while (loop_counter < 11){
-            user_input_array[loop_counter] = get_grade();
-            loop_counter++;
-        }
-    }
+    // return the user input once validated
+    return user_input;
+}
 
-    for (int x = 0; x < user_input_array.size(); x++){
-        std::cout << "\nGrade " << x + 1 << " is " << user_input_array[x];
-    }
+// function takes array of grades (after being sorted), sums 1 - 10 values, divides by size of array - 1. Lowest grade = first gets ignored. 
+int get_average (std::array <int, 11> grade_array){
 
-    // sort the array to make 0 element lowest grade
-    std::sort(user_input_array.begin(), user_input_array.end());
-
-    average_grade = get_average(user_input_array);
-
-    std::cout << "\n\nAverage after dropping lowest grade is " << average_grade << "\n\n";
-
-    student.add_final_grade(average_grade);
+    // initialize variables
+    int sum = 0;
+    int average = 0;
     
-    switch (average_grade)
-    {
-        // case x .... y will check if average_of_grades is in a range and enter a case accordingly. 
-        case 90 ... 100: 
-            student.add_final_letter_grade('A');
-            break;
-        case 80 ... 89:
-            student.add_final_letter_grade('B');
-            break;
-        case 70 ... 79:
-            student.add_final_letter_grade('C');
-            break;
-        case 60 ... 69:
-            student.add_final_letter_grade('D');
-            break;
-        default:
-            student.add_final_letter_grade('F');
-            break;
+    // loop though the array except for the first value, which will be the lowest grade
+    for (int x = 1; x < grade_array.size(); x++){
+        sum = sum + grade_array[x];
     }
 
+    // take sum from loop and divide by number of elements
+    average = sum / (grade_array.size() - 1);
 
-    std::cout << "Thank you " << student.get_name() << ", the final grade for semester is a " << student.get_final_letter_grade() << " at " << student.get_final_grade();
-
-    std::cout << "\n\n";
-
+    // return the calculated average
+    return average;
 }
--- assignments/assignment4/gradeFunction.hpp
+++ assignments/assignment4/gradeFunction.hpp
@@ -1,3 +1,9 @@
+/*
+Name: William Ward
+Course: COP3003 (CRN15015) - Programming II
+Info: Assignment 3 - gradeFunction.hpp
+*/
+
 #include <string>
 #include <array>
 #include <iostream>
@@ -22,139 +28,7 @@ struct STUDENT {
     char get_final_letter_grade();
 };
 
-// void setting data functions
-void STUDENT::add_name(std::string full_name){
-    name = full_name;
-}
-void STUDENT::add_grade_level(std::string x_grade_level){
-    grade_level = x_grade_level;
-}
-void STUDENT::add_final_grade(int x_final_grade){
-    final_grade = x_final_grade;
-}
-void STUDENT::add_final_letter_grade(char x_final_grade_letter){
-    final_letter_grade = x_final_grade_letter;
-}
-
-// return info functions
-std::string STUDENT::get_name(){
-    return name;
-}
-std::string STUDENT::get_grade_level(){
-    return grade_level;
-}
-int STUDENT::get_final_grade(){
-    return final_grade;
-}
-char STUDENT::get_final_letter_grade(){
-    return final_letter_grade;
-}
-
-void quit_grading (int x){
-
-    // switch based on input from main, once finished, the program will exit
-    switch (x){
-    case -1:
-        // reason -1
-        std::cout << "an error has occurred, please try again.\n";
-        break;
-    case 0:
-        // reason 0, terminate
-        std::cout << "Thank you for using our product!\n";
-        exit (1);
-    case 1:
-        // reason 1
-        std::cout << "Sorry you had to quit early. Thank you for using out product!\n";
-        exit (1);
-        break;
-    default: // default case just skips the switch
-        break;
-    }
-}
-
-// function is called, gets input from user, validates, returns trunc'd value if valid.
-int get_grade (){
-
-    // initialize variables
-    std::string continue_check;
-    float user_input = 0;
-    bool user_input_valid = false;
-
-    // ask user if want to continue entering grades
-    std::cout << "Do you want to continue entering grades? (y to continue)\nResponse: ";
-    std::cin >> continue_check;
-
-    // if first char of user input is y for yes, get grade
-    if (char(continue_check[0]) == 'y' || char(continue_check[0]) == 'Y'){
-        while(!user_input_valid)
-        { 
-            std::cout << "Enter grade: ";
-            std::cin >> user_input;
-
-            // if the input is within range and the cin was not passed bad input, pass true and break from nested while loop
-            if(user_input >= 0 && user_input <= 100 && std::cin.good()){ 
-                user_input_valid = true;
-            }
-
-            // else, display a message, clear cin buffer and data, repeat loop until response is valid. Stay on target... 
-            else{
-                std::cout << "please enter a valid response (0-100)\n";
-                std::cin.clear();
-                std::cin.ignore(100,'\n');
-            }
-        }
-    }
-    // if user does not give yes, quit program by passing quit_grading a 1
-    else{
-        quit_grading(1);
-    }
-    return trunc(user_input);
-}
-
-// Same as above function but will not require user specify y/n for every grade input
-int get_grade_all (){
-
-    // initialize variables
-    float user_input = 0;
-    bool user_input_valid = false;
-
-    // loop will be false until user input is validated and passed true flag
-    while(!user_input_valid){ 
-        std::cout << "Enter grade: ";
-        std::cin >> user_input;
-
-        // if the input is within range and the cin was not passed bad input, pass true and break from nested while loop
-        if(user_input >= 0 && user_input <= 100 && std::cin.good()){ 
-            user_input_valid = true;
-        }
-
-        // else, display a message, clear cin buffer and data, repeat loop until response is valid. Stay on target... 
-        else{
-            std::cout << "please enter a valid response (0-100)\n";
-            std::cin.clear();
-            std::cin.ignore(100,'\n');
-        }
-    }
-
-    // return the user input once validated
-    return user_input;
-}
-
-// function takes array of grades (after being sorted), sums 1 - 10 values, divides by size of array - 1. Lowest grade = first gets ignored. 
-int get_average (std::array <int, 11> grade_array){
-
-    // initialize variables
-    int sum = 0;
-    int average = 0;
-    
-    // loop though the array except for the first value, which will be the lowest grade
-    for (int x = 1; x < grade_array.size(); x++){
-        sum = sum + grade_array[x];
-    }
-
-    // take sum from loop and divide by number of elements
-    average = sum / (grade_array.size() - 1);
-
-    // return the calculated average
-    return average;
-}
+std::string display_and_collect(std::string);
+void quit_grading (int);
+int get_grade ();
+int get_average (std::array <int, 11> grade_array);
