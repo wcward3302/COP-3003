e4a95dd45522cb22e5031082ddd504e2726a9feb
wcward3302 <wcward3302@eagle.fgcu.edu>
2023-04-22 21:20:21 -0400

    Assignment 8 - comments, finishing touches

--- assignments/assignment8/magazine_file.cpp
+++ assignments/assignment8/magazine_file.cpp
@@ -1,40 +1,54 @@
+/*
+Name - William Ward
+Date - 4/22/23
+Assignment 8 - Inheritance
+
+Definitions of all functions needed to perform the necessary tasks
+
+*/
+
 #include "magazine_file.hpp"
 
 // -------------------------------------private member functions--------------------------------------
 
+
+// function will take vector of strings, parse out desired data, return the filled struct to the vector of structs
 void Magazine_file::split_data(){
 
     std::string line_item;
     magazine magazine_object;
     int counter = 0;
 
+    // will be using similar logic to that which was given to us for the CSV handling
     for (const auto & each : lines){
         switch(counter){
+            // logic - using substr(index, length) set index to the first instance of > + 1, and the length as the (2nd < index) - (/ index) - 2
+            // case 0 and 6 will be <magazine> lines so will increment from 0 and use 6 to push to vector of structs and reset counter
             case 0:
                 counter++;
                 break;
 
             case 1:
-                magazine_object.name = each.substr(each.find('>') + 1);
+                magazine_object.name = each.substr(each.find('>') + 1, (each.find('/') - each.find('>')) - 2);
                 counter++;
                 break;
 
             case 2:
-                magazine_object.location = each.substr(each.find('>') + 1);
+                magazine_object.location = each.substr(each.find('>') + 1, (each.find('/') - each.find('>')) - 2);
                 counter++;
                 break;
 
             case 3:
-                magazine_object.employees = std::stoi(each.substr(each.find('>') + 1));
+                magazine_object.employees = std::stoi(each.substr(each.find('>') + 1, (each.find('/') - each.find('>')) - 2));
                 counter++;
                 break;
 
             case 4:
-                magazine_object.founded = std::stoi(each.substr(each.find('>') + 1));
+                magazine_object.founded = std::stoi(each.substr(each.find('>') + 1, (each.find('/') - each.find('>')) - 2));
                 counter++;
                 break;
             case 5:
-                magazine_object.founder = each.substr(each.find('>'));
+                magazine_object.founder = each.substr(each.find('>') + 1, (each.find('/') - each.find('>')) - 2);
                 counter++;
                 break;
             case 6:
@@ -45,12 +59,28 @@ void Magazine_file::split_data(){
     }
 }
 
+// takes each struct of parsed data and appends it to a single string in the vector of strings, followed by a newline for the next struct
+void Magazine_file::to_string(){
+    for (const auto & each : magazine_list){
+        to_output.append(each.name);
+        to_output.append("\t");
+        to_output.append(each.location);
+        to_output.append("\t");
+        to_output.append(std::to_string(each.employees));
+        to_output.append("\t");
+        to_output.append(std::to_string(each.founded));
+        to_output.append("\t");
+        to_output.append(each.founder);
+        to_output.append("\n");
+    }
+}
 
 
 // --------------------------------------public member functions--------------------------------------
+
 // default constructor
 Magazine_file::Magazine_file(){
-    filename = "./magazine.xml";
+    filename = "magazine";
 }
 
 // overloaded constructor
@@ -67,7 +97,7 @@ void Magazine_file::change_filename(std::string in_file_name){
 void Magazine_file::load_file(){
 
     std::fstream file_stream;
-    file_stream.open(filename);
+    file_stream.open(filename + ".xml");
 
     if(file_stream.is_open()){
 
@@ -81,28 +111,31 @@ void Magazine_file::load_file(){
     }
 
     else{
-        std::cout << "Could not open file.";
+        std::cout << "Could not open file. (read)";
     }
 
     // calling member function to data from the lines member into the vector of structs
     split_data();
 }
 
-// take structures, create lines concat \t, output to a .txt now that all the xml has been removed
-void Magazine_file::write_file(std::string new_file_name){
+// take parsed vector of org'd strings, output to text file
+void Magazine_file::write_file(std::string out_file_name){
 
+    to_string();
 
     std::fstream file_stream;
-    file_stream.open(new_file_name);
+    file_stream.open(out_file_name + ".txt");
 
     if(file_stream.is_open()){
 
-        
+        std::cout << to_output;
+
+        file_stream << to_output;
 
         file_stream.close();
     }
 
     else{
-        std::cout << "Could not open file.";
+        std::cout << "Could not open file. (write)";
     }
 }
--- assignments/assignment8/magazine_file.hpp
+++ assignments/assignment8/magazine_file.hpp
@@ -1,3 +1,15 @@
+/*
+Name - William Ward
+Date - 4/22/23
+Assignment 8 - Inheritance
+
+prototyping header file for necessary variables and functions, set scope
+
+*/
+
+#ifndef magazine_file_hpp
+#define magazine_file_hpp
+
 #include "file.hpp"
 #include <iostream>
 
@@ -17,12 +29,16 @@ private:
     // vector to have many magazine structs
     std::vector <magazine> magazine_list;
     std::vector <std::string> lines;
+    std::string to_output;
 
     // internal class function to handle moving lines from file into data structure
     void split_data();
 
+    void to_string();
+
 public:
 
+    // default constructor
     Magazine_file();
 
     // overload constructor to accept name parameter
@@ -33,3 +49,5 @@ public:
     void load_file() override;
     void write_file(std::string) override;
 };
+
+#endif
